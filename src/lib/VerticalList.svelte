afc730c56d7fbb6c909437afa08594d9ea09b6b1
s-haya-123 <ganbaru.wakenai@gmail.com>
2024-01-02 16:39:43 +0900

    paperとGapを採用してレイアウト

---
+++ src/lib/VerticalList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let gap = 20;
+</script>
+
+<div
+    class="wrapper"
+    style:gap={`${gap}px`}
+    >
+    <slot></slot>
+</div>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,41 @@
 <script lang="ts">
 	import List, { Item, Separator, Text } from '@smui/list';
+    import Paper, { Title, Subtitle, Content } from '@smui/paper';
+    import VerticalList from '$lib/VerticalList.svelte';
 
 	let clicked = 'nothing yet';
 </script>
 
 <a href="/detail/1">test</a>
 
-<div>
-	<List class="demo-list">
-		<Item on:SMUI:action={() => (clicked = 'Cut')}><Text>Cut</Text></Item>
-		<Item on:SMUI:action={() => (clicked = 'Copy')}><Text>Copy</Text></Item>
-		<Item on:SMUI:action={() => (clicked = 'Paste')}><Text>Paste</Text></Item>
-		<Separator />
-		<Item on:SMUI:action={() => (clicked = 'Delete')}><Text>Delete</Text></Item>
-	</List>
-</div>
-
+<VerticalList gap={30}>
+    <div class="item">
+        <Paper>
+            <Title>新規登録</Title>
+        </Paper>
+    </div>
+    <div class="item">
+        <Paper>
+            <Title>
+                一覧
+            </Title>
+            <Content>
+                <List class="demo-list">
+                    <Separator />
+                    <Item on:SMUI:action={() => (clicked = 'Cut')}><Text>Cut</Text></Item>
+                    <Item on:SMUI:action={() => (clicked = 'Copy')}><Text>Copy</Text></Item>
+                    <Item on:SMUI:action={() => (clicked = 'Paste')}><Text>Paste</Text></Item>
+                    <Item on:SMUI:action={() => (clicked = 'Delete')}><Text>Delete</Text></Item>
+                </List>
+            </Content>
+        </Paper>
+    </div>
+</VerticalList>
+    
 <pre class="status">Clicked: {clicked}</pre>
 
 <style>
-	* :global(.demo-list) {
-		max-width: 300px;
-	}
+    .item {
+        width: 100%;
+    }
 </style>
